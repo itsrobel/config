c916d68eba9b0f8c335c194b49559367a16e56f1
Amadeus-Stephen <itsrobel.schwarz@gmail.com>
2021-05-29 23:56:07 -0400

    added fish

---
+++ arch-systems/fish/config.fish
@@ -0,0 +1,14 @@
+
+#genral shortcuts
+alias l='ls -lah'
+alias ssn='sudo shutdown now'
+alias sr='sudo reboot'
+
+#code shortcuts
+alias gitcom="git add . && git commit -m 'update/fixes' && git push -u origin master"
+
+
+
+
+#scripts
+alias mi='cd ~/Documents/scripts/node-dl/ && node index.js && cd ~/'
