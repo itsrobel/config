969244d945916c7cc9c83e63de521c592de9619a
Amar Shavir <itsrobel.schwarz@gmail.com>
2023-12-16 16:41:03 -0500

    update

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo pacman-mirrors --fasttrack && sudo pacman -Syu
 sudo pacman -Syyu
-sudo pacman -S awesome qtile rofi nitrogen base-devel obsidian emacs dracula-icons-git lxapperance dracula-gtk-theme eca spotify microsoft-edge-stable-bin google-messages
+sudo pacman -S awesome qtile paru rofi nitrogen base-devel obsidian emacs dracula-icons-git  dracula-gtk-theme eca spotify microsoft-edge-stable-bin google-messages
 
 
 sudo chmod a+wr /opt/spotify
@@ -20,14 +20,18 @@ cp -r ~/Documents/config/.face ~/
 cp -r ~/Documents/config/alacritty ~/.config/
 cp -r ~/Documents/config/fish ~/.config/
 cp -r ~/Documents/config/rofi ~/.config/
+cp -r ~/Documents/config/qtile ~/.config/
 cp -r ~/Documents/config/.doom.d ~/.config/
+cp -r ~/Documents/config/awesome ~/.config/
+
+
+
 cp -r ~/Documents/config/spicetify/Dracula ~/.config/spicetify/Themes/
 
 spicetify config current_theme Dracula
 spicetify backup apply
 spicetify apply
 
-cp -r ~/Documents/config/awesome ~/.config/
 chsh -s $(which fish)
 git config --global user.email "itsrobel.schwarz@gmail.com" && git config --global user.name "Robel Schwarz"
 #sudo reboot
