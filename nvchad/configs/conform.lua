4db8c74bd794e0b7e110ef3d9fddb75a68690416
robel schwarz <itsrobel.schwarz@gmail.com>
2024-02-29 03:27:40 -0500

    python language server stuff

--- nvchad/configs/conform.lua
+++ nvchad/configs/conform.lua
@@ -4,20 +4,20 @@ local options = {
 
 	formatters_by_ft = {
 		lua = { "stylua" },
-
+		python = { "black" },
 		javascript = { "prettier" },
 		css = { "prettier" },
 		html = { "prettier" },
-    markdown = {"prettier"},
+		markdown = { "prettier" },
 		sh = { "shfmt" },
 	},
 
-  -- adding same formatter for multiple filetypes can look too much work for some
-  -- instead of the above code you could just use a loop! the config is just a table after all!
+	-- adding same formatter for multiple filetypes can look too much work for some
+	-- instead of the above code you could just use a loop! the config is just a table after all!
 
 	format_on_save = {
-	  timeout_ms = 500,
-	  lsp_fallback = true,
+		timeout_ms = 500,
+		lsp_fallback = true,
 	},
 }
 
