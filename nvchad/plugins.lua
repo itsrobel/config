b33f4c30c3a6b8bcedcea0b97be94895dfbce5b1
robel schwarz <itsrobel.schwarz@gmail.com>
2024-03-03 00:36:59 -0500

    add latex to treesitter

--- nvchad/plugins.lua
+++ nvchad/plugins.lua
@@ -134,11 +134,12 @@ local plugins = {
 			sources = {
 				{ name = "copilot", group_index = 2 },
 				{ name = "nvim_lsp", group_index = 2 },
+				{ name = "mkdnflow", group_index = 2 },
+				{ name = "emoji", group_index = 2 },
 				{ name = "luasnip", group_index = 2 },
 				{ name = "buffer", group_index = 2 },
 				{ name = "nvim_lua", group_index = 2 },
 				{ name = "path", group_index = 2 },
-				{ name = "mkdnflow", group_index = 2 },
 			},
 		},
 	},
