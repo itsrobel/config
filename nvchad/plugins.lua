60c0ea49694bcd84553677c1f371124d99b7e6fc
Robel Schwarz <itsrobel.schwarz@gmail.com>
2024-03-04 22:10:05 -0500

    I did something?

--- nvchad/plugins.lua
+++ nvchad/plugins.lua
@@ -160,7 +160,7 @@ local plugins = {
 		dependencies = { "L3MON4D3/LuaSnip" },
 		config = function()
 			require("luasnip-latex-snippets").setup({ use_treesitter = true, allow_on_markdown = true })
-			require("luasnip").config.setup({ enable_autosnippets = true })
+			require("luasnip").config.setup({ enable_autosnippets = false })
 		end,
 		lazy = false,
 	},
