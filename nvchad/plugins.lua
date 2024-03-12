e2f3ccd6ef1bfbf428110eb8826638d3138d46f4
robel schwarz <itsrobel.schwarz@gmail.com>
2024-03-11 22:26:22 -0400

    added chat gpt to neovim config

--- nvchad/plugins.lua
+++ nvchad/plugins.lua
@@ -108,6 +108,24 @@ local plugins = {
 		"folke/zen-mode.nvim",
 	},
 
+	-- AI
+	{
+		"jackMort/ChatGPT.nvim",
+		event = "VeryLazy",
+		config = function()
+			local home = vim.fn.expand("$HOME")
+			require("chatgpt").setup({
+				api_key_cmd = "cat " .. home .. "/config/keychain/decrypted/neogpt.txt",
+			})
+		end,
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"folke/trouble.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+
 	-- Markdown
 	{
 		"lukas-reineke/headlines.nvim",
@@ -175,6 +193,10 @@ local plugins = {
 	--   "mg979/vim-visual-multi",
 	--   lazy = false,
 	-- }
+	{
+		"windwp/nvim-autopairs",
+		ft = { "disable" },
+	},
 }
 
 return plugins
