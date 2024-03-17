75461902cf2d5d229e64f288b86833d8d8a8f463
robel schwarz <itsrobel.schwarz@gmail.com>
2024-03-17 04:27:49 -0400

    things

--- nvchad/chadrc.lua
+++ nvchad/chadrc.lua
@@ -1,21 +1,20 @@
 ---@type ChadrcConfig
 
 local M = {}
-
 -- Path to overriding theme and highlights files
 local highlights = require("custom.highlights")
 
 M.ui = {
-    theme = "gruvbox",
-    theme_toggle = { "gruvbox", "gruvbox_light" },
+  theme = "gruvbox",
+  theme_toggle = { "gruvbox", "gruvbox_light" },
 
-    hl_override = highlights.override,
-    hl_add = highlights.add,
-    statusline = { theme = "minimal" },
-    tabufline = {
-        enabled = false,
-    },
-    nvdash = { load_on_startup = true },
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+  statusline = { theme = "minimal" },
+  tabufline = {
+    enabled = false,
+  },
+  nvdash = { load_on_startup = true },
 }
 
 M.plugins = "custom.plugins"
