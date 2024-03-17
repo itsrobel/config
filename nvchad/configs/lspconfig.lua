dcd38d7f8bb5927872bc5c43a040b5380e3e9130
robel schwarz <itsrobel.schwarz@gmail.com>
2024-03-16 22:11:04 -0400

    tabs are now not messed with by nvim cmp

--- nvchad/configs/lspconfig.lua
+++ nvchad/configs/lspconfig.lua
@@ -1,6 +1,5 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
-
 local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
@@ -19,3 +18,22 @@ end
 
 --
 -- lspconfig.pyright.setup { blabla}
+
+lspconfig.gopls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "gopls" },
+	filetype = { "go", "gomod", "gowork", "gotmpl" },
+	root_dir = lspconfig.util.root_pattern("go.mod", ".git", "go.work"),
+	settings = {
+		gopls = {
+			completeUnimported = true,
+			usePlaceholders = true,
+			analyses = {
+				unusedparams = true,
+				shadow = true,
+			},
+			staticcheck = true,
+		},
+	},
+})
--- nvchad/mappings.lua
+++ nvchad/mappings.lua
@@ -4,100 +4,124 @@
 local M = {}
 
 M.general = {
-    n = {
-        [";"] = { ":", "enter command mode", opts = { nowait = true } },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
 
-        --  format with conform
-        ["<leader>fm"] = {
-            function()
-                require("conform").format()
-            end,
-            "formatting",
-        },
-        ["<leader>sc"] = {
-            ":setlocal spell! spelllang=en_us<CR>",
-        },
-        ["<leader>z"] = {
-            function()
-                require("zen-mode").toggle()
-            end,
-            "Zen Mode",
-        },
-        ["<C-s>"] = { ":Telescope FIles <CR>", "Telescope Files" },
+		--  format with conform
+		["<leader>fm"] = {
+			function()
+				require("conform").format()
+			end,
+			"formatting",
+		},
+		["<leader>sc"] = {
+			":setlocal spell! spelllang=en_us<CR>",
+		},
+		["<leader>z"] = {
+			function()
+				require("zen-mode").toggle()
+			end,
+			"Zen Mode",
+		},
+		["<C-s>"] = { ":Telescope FIles <CR>", "Telescope Files" },
 
-        ["<M-k>"] = {
-            function()
-                require("moveline").up()
-            end,
-        },
-        ["<M-j>"] = {
-            function()
-                require("moveline").down()
-            end,
-        },
+		["<M-k>"] = {
+			function()
+				require("moveline").up()
+			end,
+		},
+		["<M-j>"] = {
+			function()
+				require("moveline").down()
+			end,
+		},
 
-        ["<C-.>"] = {
-            "<cmd>vertical resize +5<cr>",
-            "+Window Resize",
-        },
-        ["<C-,>"] = {
-            "<cmd>vertical resize -5<cr>",
-            "-Window Resize",
-        },
+		["<C-.>"] = {
+			"<cmd>vertical resize +5<cr>",
+			"+Window Resize",
+		},
+		["<C-,>"] = {
+			"<cmd>vertical resize -5<cr>",
+			"-Window Resize",
+		},
 
-        -- ["<leader>.t0"] = { "<cmd>let g:neovide_transparency=1.0<cr>", "Transparency off" },
-    },
-    v = {
-        [">"] = { ">gv", "indent" },
+		-- ["<leader>.t0"] = { "<cmd>let g:neovide_transparency=1.0<cr>", "Transparency off" },
+	},
+	v = {
+		[">"] = { ">gv", "indent" },
 
-        ["<M-k>"] = {
-            function()
-                require("moveline").block_up()
-            end,
-        },
-        ["<M-j>"] = {
-            function()
-                require("moveline").block_down()
-            end,
-        },
-    },
+		["<M-k>"] = {
+			function()
+				require("moveline").block_up()
+			end,
+		},
+		["<M-j>"] = {
+			function()
+				require("moveline").block_down()
+			end,
+		},
+	},
 }
 M.dap = {
-    plugin = true,
-    n = {
-        ["<leader>db"] = {
-            "<cmd> DapToggleBreakpoint<CR>",
-            "Add breakpoint at line",
-        },
-        ["<leader>dc"] = {
-            "<cmd> DapContinue<CR>",
-            "Continue",
-        },
-        ["<leader>dr"] = {
-            "<cmd> DapRestart<CR>",
-            "Restart",
-        },
-        ["<leader>dn"] = {
-            "<cmd> DapNext<CR>",
-            "Next",
-        },
-        ["<leader>di"] = {
-            "<cmd> DapStepInto<CR>",
-            "Step Into",
-        },
-        ["<leader>do"] = {
-            "<cmd> DapStepOut<CR>",
-            "Step Out",
-        },
-        ["<leader>ds"] = {
-            "<cmd> DapStop<CR>",
-            "Stop",
-        },
-        ["<leader>dl"] = {
-            "<cmd> DapList<CR>",
-            "List",
-        },
-    },
+	plugin = true,
+	n = {
+		["<leader>db"] = {
+			"<cmd> DapToggleBreakpoint<CR>",
+			"Add breakpoint at line",
+		},
+		["<leader>dc"] = {
+			"<cmd> DapContinue<CR>",
+			"Continue",
+		},
+		-- ["<leader>di"] = {
+		--     "<cmd> DapStepInto<CR>",
+		--     "Step Into",
+		-- },
+		-- ["<leader>do"] = {
+		--     "<cmd> DapStepOut<CR>",
+		--     "Step Out",
+		-- },
+		["<leader>dt"] = {
+			"<cmd> DapTerminate<CR>",
+			"Stop",
+		},
+		["<leader>dus"] = {
+			function()
+				local widgets = require("dap.ui.widgets")
+				local siderbar = widgets.sidebar(widgets.scopes)
+				siderbar.open()
+			end,
+		},
+	},
+}
+
+M.dap_go = {
+	plugin = true,
+	n = {
+		["<leader>dgt"] = {
+			function()
+				require("dap-go").debug_test()
+			end,
+			"debug go test",
+		},
+		["<leader>dgl"] = {
+			function()
+				require("dap-go").debug_last()
+			end,
+			"Debug last go test",
+		},
+	},
+}
+M.gopher = {
+	plugin = true,
+	n = {
+		["<leader>gsj"] = {
+			"<cmd> GoTagAdd json <CR>",
+		},
+		["<leader>gsy"] = {
+			"<cmd> GoTagAdd yaml <CR>",
+		},
+	},
 }
 
 -- M.abc = {
