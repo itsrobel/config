5aa131b9fded34ffa1d078e33bb816c493ef5873
robel schwarz <itsrobel.schwarz@gmail.com>
2024-03-17 23:58:18 -0400

    added nvchad config folder back

---
+++ nvchad/configs/conform.lua
@@ -0,0 +1,25 @@
+--type conform.options
+local options = {
+	lsp_fallback = true,
+
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "black" },
+		javascript = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+		markdown = { "prettier" },
+		sh = { "shfmt" },
+		cpp = { "clang-format" },
+	},
+
+	-- adding same formatter for multiple filetypes can look too much work for some
+	-- instead of the above code you could just use a loop! the config is just a table after all!
+
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
+}
+
+require("conform").setup(options)
