cf924b3f6f8cee04cd5d930aa6ecd798483b66e3
Robel Schwarz <itsrobel.schwarz@gmail.com>
2024-03-19 07:56:01 -0400

    all the script changes

--- scripts/sysd.sh
+++ scripts/sysd.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
+
+sudo systemctl enable auto-cpufreq
+sudo systemctl enable bluetooth
 sudo systemctl enable bluetooth-autoconnect
 sudo systemctl enable sshd
-sudo systemctl enable libvirtd
-sudo systemctl enable NetworkManager
+# sudo systemctl enable libvirtd
+# sudo systemctl enable NetworkManager
 
 # sudo systemctl enable docker
 # sudo systemctl enable firewalld
