545968f7b5ba8766a3ec97f6b5479287a95ac83f
robel schwarz <itsrobel.schwarz@gmail.com>
2024-03-19 02:31:43 -0400

    none-ls migration

--- nvchad/plugins/null-ls.lua
+++ nvchad/plugins/null-ls.lua
@@ -1,38 +1,20 @@
 return {
-	"jose-elias-alvarez/null-ls.nvim",
+	"nvimtools/none-ls.nvim",
 	event = "VeryLazy",
 	opts = function(_, opts)
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 		local null_ls = require("null-ls")
 		opts.sources = {
+			null_ls.builtins.formatting.prettier,
 			null_ls.builtins.formatting.clang_format,
 			null_ls.builtins.formatting.gofumpt,
 			null_ls.builtins.formatting.goimports_reviser,
 			null_ls.builtins.formatting.golines,
 			null_ls.builtins.formatting.stylua,
-			null_ls.builtins.formatting.prettier,
-			null_ls.builtins.formatting.eslint_d,
+			null_ls.builtins.completion.spell,
+			null_ls.builtins.diagnostics.mypy,
 			null_ls.builtins.formatting.black,
 			null_ls.builtins.formatting.isort,
-			null_ls.builtins.completion.spell,
-			null_ls.builtins.diagnostics.shellcheck,
-			null_ls.builtins.diagnostics.mypy,
-			null_ls.builtins.diagnostics.ruff,
 		}
-		opts.on_attach = function(client, bufnr)
-			if client.supports_method("textDocument/formatting") then
-				vim.api.nvim_clear_autocmds({
-					group = augroup,
-					buffer = bufnr,
-				})
-				vim.api.nvim_create_autocmd("BufWritePre", {
-					group = augroup,
-					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.formatting({ bufnr = bufnr })
-					end,
-				})
-			end
-		end
 	end,
 }
