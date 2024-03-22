a4229ad48145646b6ef9f5c761236dce38deeabf
robel schwarz <itsrobel.schwarz@gmail.com>
2024-03-22 01:34:51 -0400

    add python config

--- nvchad/plugins/mason.lua
+++ nvchad/plugins/mason.lua
@@ -1,41 +1,43 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      -- lua stuff
-      "lua-language-server",
+	"williamboman/mason.nvim",
+	opts = {
+		ensure_installed = {
+			-- lua stuff
+			"lua-language-server",
 
-      -- java
-      "jdtls",
-      "java-language-server",
-      "stylua",
-      "julia-lsp",
+			-- java
+			"jdtls",
+			"java-language-server",
+			"stylua",
+			"julia-lsp",
 
-      -- python
-      "black",
-      "mypy",
-      "ruff",
-      "pyright",
-      "isort",
-      -- web dev stuff
-      "css-lsp",
-      "html-lsp",
-      "typescript-language-server",
-      "deno",
-      "prettier",
-      -- c/cpp stuff
-      "clangd",
-      "clang-format",
-      "codelldb",
-      --go
-      "gopls",
-      -- shell stuff
-      "shfmt",
-      -- other
-      "impl",
-      --markdown
-      "ltex-ls",
-      "yaml-language-server",
-    },
-  },
+			-- python
+			"black",
+			"mypy",
+			-- "ruff",
+			"ruff-lsp",
+			"pyright",
+			"pyright",
+			"isort",
+			-- web dev stuff
+			"css-lsp",
+			"html-lsp",
+			"typescript-language-server",
+			"deno",
+			"prettier",
+			-- c/cpp stuff
+			"clangd",
+			"clang-format",
+			"codelldb",
+			--go
+			"gopls",
+			-- shell stuff
+			"shfmt",
+			-- other
+			"impl",
+			--markdown
+			"ltex-ls",
+			"yaml-language-server",
+		},
+	},
 }
