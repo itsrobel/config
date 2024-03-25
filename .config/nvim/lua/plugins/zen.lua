c6b3b9f06c9b699f83636f342bd06dc5b063ec5d
robel schwarz <itsrobel.schwarz@gmail.com>
2024-03-25 00:34:22 -0400

    disabled flash

---
+++ .config/nvim/lua/plugins/zen.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/zen-mode.nvim",
+  keys = {
+    {
+      "<leader>cz",
+      function()
+        require("zen-mode").toggle()
+      end,
+      desc = "ZenMode",
+    },
+  },
+}
