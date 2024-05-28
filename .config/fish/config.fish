d99ded87d43b7a1422c6c8e1e1489e1bda108ee2
Robel Schwarz <scrobel@amazon.com>
2024-05-28 14:50:56 -0700

    added tmux updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,8 +13,15 @@ alias ssn='sudo shutdown now'
 alias sr='sudo reboot'
 alias rd='rm -rf'
 alias on="cd ~/Documents/notes && nvim"
+alias vim="nvim"
+
 alias oc="cd ~/config && nvim"
 alias doom='~/.config//emacs/bin/doom'
+
+
+
+
+
 
 alias update="paru -Syyu"
 alias unlock="sudo rm /var/lib/pacman/db.lck"
