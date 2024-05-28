81f6065df3d3d3d937762fbd94d6b76da845b1af
Robel Schwarz <scrobel@amazon.com>
2024-05-28 14:52:43 -0700

    updated mac nvim

--- .config/nvim/lua/plugins/telescope.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "nvim-telescope/telescope.nvim",
-  keys = {
-
-    { "<C-f>", ":Telescope find_files <CR>", "Telescope Files" },
-    { "<C-s>", ":Telescope current_buffer_fuzzy_find <CR>", "Telescope Find In Current Buffer" },
-  },
-}
