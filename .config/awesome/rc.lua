018be2c2e265917fec55dbc92a8b1d2b854711eb
Robel Schwarz <itsrobel.schwarz@gmail.com>
2024-07-24 03:32:54 -0400

    some changes?

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -63,7 +63,7 @@ run_once({ "urxvtd", "unclutter -root" }) -- comma-separated entries
 
 local modkey = "Mod4"
 local altkey = "Mod1"
-local terminal = "kitty -e tmux attach -t father"
+local terminal = "kitty -e tmux new-session -A -s father"
 local vi_focus = false -- vi-like client focus https://github.com/lcpz/awesome-copycats/issues/275
 -- local cycle_prev   = true  -- cycle with only the previously focused client or all https://github.com/lcpz/awesome-copycats/issues/274
 -- local nvim = "kitty -e (tmux -e nvim)"
