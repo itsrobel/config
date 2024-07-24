fbea3da5eab449a69c18cbfacf16207f7e6c783b
Robel Schwarz <itsrobel.schwarz@gmail.com>
2024-07-23 21:12:15 -0400

    updating neovim to use tokyonight

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,21 @@
 return {
-  { "https://github.com/ellisonleao/gruvbox.nvim" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "moon",
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+      keywords = { italic = true },
+      comments = { bold = true },
+      functions = { italic = true },
+      variables = { italic = false },
     },
   },
+
+  init = function()
+    vim.cmd.colorscheme("tokyonight")
+  end,
 }
