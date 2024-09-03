90127f320f69e70d10e5c497937a30826d87173f
Robel Schwarz <scrobel@amazon.com>
2024-09-03 10:12:43 -0700

    removed plugins

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -189,15 +189,14 @@ return { -- LSP Configuration & Plugins
           useLibraryCodeForTypes = true,
         },
       },
-      rust_analyzer = {},
+      -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
-      tsserver = {},
-      astro = {},
+      -- tsserver = {},
 
       lua_ls = {
         -- cmd = {...},
