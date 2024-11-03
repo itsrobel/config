342b8171d3c6229d8b4bed2caabfff915dc37fda
Robel Schwarz <itsrobel.schwarz@gmail.com>
2024-11-03 13:56:10 -0800

    fix the lid functions later

--- .config/hypr/lid-open.sh
+++ .config/hypr/lid-open.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 hyprctl keyword monitor "eDP-1,preferred,auto,1.25"
-
 if ! hyprctl monitors | grep -q "DP-[0-9]\|HDMI-[0-9]"; then
-  # No external monitor, lock the screen
   hyprlock
 fi
