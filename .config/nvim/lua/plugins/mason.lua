6a04e8eab7f7bc60d500b5f9771b86912135a249
Robel Schwarz <itsrobel.schwarz@gmail.com>
2024-11-03 11:27:54 -0800

    some random changes somewhere

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -10,6 +10,9 @@ return {
         enabled = false,
         -- enabled = vim.g.lazyvim_python_lsp == "basedpyright",
       },
+      clangd = {
+        filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "hpp" },
+      },
 
       ruff_lsp = {
         keys = {
