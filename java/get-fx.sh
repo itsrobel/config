c5fdec966e020869baab2b7e60bc53e197d03e64
Robel Schwarz <itsrobel.schwarz@gmail.com>
2024-12-16 22:56:02 +0000

    msc

--- java/get-fx.sh
+++
@@ -1,4 +0,0 @@
-curl -o javafx-sdk-21.zip https://download2.gluonhq.com/openjfx/21.0.2/openjfx-21.0.2_linux-x64_bin-sdk.zip 
-unzip javafx-sdk-21.zip
-mv javafx-sdk-21.0.2 ~/config/java/
-rm javafx-sdk-21.zip
